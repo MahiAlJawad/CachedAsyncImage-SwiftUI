1c9a038f8a1bdaaf14fa52b8fd962047aea7df60
MahiAlJawad <br.mahialjawad@gmail.com>
2024-09-08 14:23:02 +0600

    Added CachedAsyncImage library

--- CachedAsyncImage/ContentView.swift
+++ CachedAsyncImage/ContentView.swift
@@ -8,14 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
+    let photoURLString = "https://images.unsplash.com/5/unsplash-kitsune-4.jpg?ixlib=rb-0.3.5&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjEyMDd9&s=ce40ce8b8ba365e5e6d06401e5485390"
+    
+    var photoURL: URL? {
+        URL(string: photoURLString)
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        AsyncCachedImage(url: photoURL) { image in
+            image
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+        } placeholder: {
+            ProgressView()
         }
-        .padding()
     }
 }
 
