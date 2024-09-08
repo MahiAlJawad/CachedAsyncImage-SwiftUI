17b573eb6171166a28753096d2f92efea117d00b
MahiAlJawad <br.mahialjawad@gmail.com>
2024-09-08 13:57:07 +0600

    Initial Commit

---
+++ CachedAsyncImage/CachedAsyncImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  CachedAsyncImageApp.swift
+//  CachedAsyncImage
+//
+//  Created by Mahi Al Jawad on 8/9/24.
+//
+
+import SwiftUI
+
+@main
+struct CachedAsyncImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CachedAsyncImage/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CachedAsyncImage
+//
+//  Created by Mahi Al Jawad on 8/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
